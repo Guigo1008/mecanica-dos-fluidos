a1dc96d60e3ecb2f49ce8577d547cd51c94eaa17
guigo1008 <guigo@eq.ufrj.br>
2024-03-23 22:20:40 -0300

    Fix: A substituido por Link (recomendado pelo next)

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import { Header } from "../components/Header";
 
 export default function Home() {
@@ -8,31 +9,36 @@ export default function Home() {
       <div className="flex mx-6">
       <aside className="flex flex-col gap-4 px-10 pr-40 items-center justify-start">
           <h3 className="text-2xl font-serif">Capítulos</h3>
-          <a href="/capitulos/capitulo1" className=" text-lg font-thin hover:text-yellow-300">
+          <Link href="/capitulos/capitulo1" className=" text-lg font-thin hover:text-yellow-300">
             Capítulo 1
-          </a>
-          <a href="/capitulos/capitulo2" className="text-lg font-thin hover:text-yellow-300">
+          </Link>
+          <Link href="/capitulos/capitulo2" className="text-lg font-thin hover:text-yellow-300">
             Capítulo 2
-          </a>
-          <a href="/capitulos/capitulo3" className="text-lg font-thin hover:text-yellow-300">
+          </Link>
+          <Link href="/capitulos/capitulo3" className="text-lg font-thin hover:text-yellow-300">
             Capítulo 3
-          </a>
-          <a href="/capitulos/capitulo4" className="text-lg font-thin hover:text-yellow-300">
+          </Link>
+          <Link href="/capitulos/capitulo4" className="text-lg font-thin hover:text-yellow-300">
             Capítulo 4
-          </a>
-          <a href="/capitulos/capitulo5" className="text-lg font-thin hover:text-yellow-300">
+          </Link>
+          <Link href="/capitulos/capitulo5" className="text-lg font-thin hover:text-yellow-300">
             Capítulo 5
-          </a>
-          <a href="/capitulos/capitulo6" className="text-lg font-thin hover:text-yellow-300">
+          </Link>
+          <Link href="/capitulos/capitulo6" className="text-lg font-thin hover:text-yellow-300">
             Capítulo 6
-          </a>
+          </Link>
         </aside>
         <div className="flex flex-col w-full items-center justify-center gap-5">
           <h1 className="text-center border-2 border-purple-400 rounded-lg p-4 text-4xl hover:text-yellow-300">
             Mecânica dos Fluidos</h1>
           <p className="hover:text-yellow-300">Professor: Luís Fernando</p>
-          <video src={require("../public/video-background.mp4")} autoPlay loop muted 
-          className="absolute top-0 left-0 w-full h-full object-cover z-[-1] animate-fade-in-out"></video>
+          <video
+            src="/video-background.mp4"
+            autoPlay
+            loop
+            muted
+            className="absolute top-0 left-0 w-full h-full object-cover z-[-1] animate-fade-in-out"
+          />
         </div>
       </div>
     </main>
--- components/Header.js
+++ components/Header.js
@@ -1,21 +1,22 @@
 "use client";
+import Link from "next/link";
 
 export const Header = () => {
   return (
     <header className="w-full flex flex-row p-3 items-center justify-between border-purple-400 border-b mb-3">
-      <a href="/">
+      <Link href="/">
         <img 
           src={"/fluid-mech-icon.png"} 
           width={25}
           alt="Simbolo de representação da mecânica dos fluidos."
           className="mx-3" 
         />
-      </a>
+      </Link>
       <nav className="flex gap-5 text-purple-950 mx-10">
-        <a href="/" className="hover:text-yellow-300">Home</a>
-        <a href="/resumos" className="hover:text-yellow-300">Resumos</a>
-        <a href="/exercicios" className="hover:text-yellow-300">Exercícios</a>
-        <a href="/sobre" className="hover:text-yellow-300">Sobre</a>
+        <Link href="/" className="hover:text-yellow-300">Home</Link>
+        <Link href="/resumos" className="hover:text-yellow-300">Resumos</Link>
+        <Link href="/exercicios" className="hover:text-yellow-300">Exercícios</Link>
+        <Link href="/sobre" className="hover:text-yellow-300">Sobre</Link>
       </nav>
     </header>
 )};
